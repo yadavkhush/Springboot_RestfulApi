3506b0e5289a01b8fcba30f8f86e8e2df01c36ab
khushboo <129037576+yadavkhush@users.noreply.github.com>
2024-01-09 01:34:56 +0530

    Delete employee-rest-api (2) directory

--- employee-rest-api (2)/employee-rest-api/src/main/java/com/employee/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.employee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.employee.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-
-}
